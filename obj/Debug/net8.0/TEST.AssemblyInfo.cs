be2e80f1c5aaef78bdd9350c8096b762b536a6a3
CJ-REYES <cjosereyes09@gmail.com>
2024-11-21 12:06:57 -0600

    CREACION DE CONTROLLERS

--- obj/Debug/net8.0/TEST.AssemblyInfo.cs
+++ obj/Debug/net8.0/TEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2753c5aed183da01bf22f4439dc9691af621ac2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7ed10ac7fcefb3487a18e337470fb62c81ba49f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
